e5ee355332e54687af2c86b0c01dfb7ea8369cb4
nruest <ruestn@gmail.com>
2012-09-05 17:13:58 -0400

    rails setup - framing

---
+++ RoR/glacier-bagit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlacierBagit::Application.config.secret_token = 'ef9ece69c36a1fe8380e2c6617e2a8b3afaf8db7083d697c68f385fbe207b6303717a65812ef175f61bea6ca3506766586b1287b5cf24a2c438efcd93d3be8a5'
